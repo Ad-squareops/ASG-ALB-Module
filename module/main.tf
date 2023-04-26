bc74eaf3c8571402e72a7a334a8f1ab3ee0ba4aa
Ad-squareops <118443159+Ad-squareops@users.noreply.github.com>
2023-04-26 15:08:54 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -53,12 +53,12 @@ module "asg" {
   min_size                  = var.min_size
   max_size                  = var.max_size
   desired_capacity          = var.desired_capacity
-  vpc_zone_identifier       = var.vpc_zone_identifier
-  wait_for_capacity_timeout = var.wait_for_capacity_timeout
-  target_group_arns         = var.alb_enable ? module.alb[0].target_group_arns : var.target_group_arn
+  vpc_zone_identifier       = var.private_subnets
+  wait_for_capacity_timeout = 0
+  target_group_arns         = module.alb.target_group_arns
   health_check_type         = var.health_check_type
-  default_instance_warmup   = var.default_instance_warmup
-  enabled_metrics           = var.enabled_metrics
+  default_instance_warmup   = 300
+  enabled_metrics = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupPendingInstances", "GroupStandbyInstances", "GroupTerminatingInstances", "GroupTotalInstances"]
 
   instance_refresh = {
     strategy = "Rolling"
@@ -74,12 +74,12 @@ module "asg" {
  # Launch template
   launch_template_name         = "${var.app_name}-lt"
   launch_template_description  = "Launch template example"
-  update_default_version       = var.update_default_version
+  update_default_version       = true
   image_id                     = var.image_id
   instance_type                = var.instance_type
   key_name                     = module.key_pair.key_pair_name
-  ebs_optimized                = var.ebs_optimized
-  enable_monitoring            = var.enable_monitoring
+  ebs_optimized                = true
+  enable_monitoring            = true
   security_groups              = [aws_security_group.asg-sg.id]
   iam_instance_profile_name    = aws_iam_instance_profile.instance-profile.name
 
@@ -90,6 +90,71 @@ module "asg" {
   }
 }
 
+
+
+# Scaling Policies
+# disk usage 
+resource "aws_autoscaling_policy" "scale_up_on_high_disk_usage" {
+  count                  = var.asg_scale_updown_disk_usage_policy ? 1 : 0
+  name                   = "asg-disk-scale-up-policy"
+  autoscaling_group_name = module.asg.autoscaling_group_name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = "1"
+  cooldown               = "300"
+  policy_type            = "SimpleScaling"
+}
+
+resource "aws_cloudwatch_metric_alarm" "disk_usage_high_alarm" {
+  count               = var.asg_scale_updown_disk_usage_policy ? 1 : 0
+  alarm_name          = "asg-scale-up-alarm"
+  alarm_description   = "asg-scale-up-disk-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "LogicalDisk % Used Space"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = var.disk_threshold_to_scale_up
+  dimensions = {
+    "AutoScalingGroupName" = module.asg.autoscaling_group_name
+  }
+  actions_enabled = true
+  alarm_actions   = [aws_autoscaling_policy.scale_up_on_high_disk_usage[0].arn]
+  depends_on      = [aws_autoscaling_policy.scale_up_on_high_disk_usage]
+}
+
+
+resource "aws_autoscaling_policy" "scale_down_on_low_disk_usage" {
+  count                  = var.asg_scale_updown_disk_usage_policy ? 1 : 0
+  name                   = "asg-disk-scale-down-policy"
+  autoscaling_group_name = module.asg.autoscaling_group_name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = "-1"
+  cooldown               = "300"
+  policy_type            = "SimpleScaling"
+}
+
+resource "aws_cloudwatch_metric_alarm" "disk_usage_low_alarm" {
+  count               = var.asg_scale_updown_disk_usage_policy ? 1 : 0
+  alarm_name          = "asg-scale-down-alarm"
+  alarm_description   = "asg-scale-down-disk-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "LogicalDisk % Used Space"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = var.disk_threshold_to_scale_down
+  dimensions = {
+    "AutoScalingGroupName" = module.asg.autoscaling_group_name
+  }
+  actions_enabled = true
+  alarm_actions   = [aws_autoscaling_policy.scale_down_on_low_disk_usage[0].arn]
+  depends_on      = [aws_autoscaling_policy.scale_down_on_low_disk_usage]
+}
+
+
+# ASGAverageCPUUtilization
 resource "aws_autoscaling_policy" "asg_cpu_policy" {
   count                     = var.asg_cpu_policy ? 1 : 0
   name                      = "${var.app_name}-cpu-policy"
@@ -104,23 +169,26 @@ resource "aws_autoscaling_policy" "asg_cpu_policy" {
   }
 }
 
+# ALBRequestCountPerTarget
 resource "aws_autoscaling_policy" "asg_ALB_request_count_policy" {
-  count                     = var.asg_ALB_request_count_policy ? 1 : 0
-  name                      = "${var.app_name}-cpu-policy"
+  depends_on                = [module.alb]
+  #count                     = var.asg_ALB_request_count_policy ? 1 : 0
+  name                      = "${var.app_name}-ALBRequestCountPerTarget-policy"
   autoscaling_group_name    = module.asg.autoscaling_group_name
   estimated_instance_warmup = 60
   policy_type               = "TargetTrackingScaling"
   target_tracking_configuration {
     predefined_metric_specification {
       predefined_metric_type = "ALBRequestCountPerTarget"
+      resource_label = "${module.alb.lb_arn_suffix}/${module.alb.target_group_arn_suffixes[0]}" 
     }
-    target_value = var.request_count_value_threshold
+    target_value = var.target_value
   }
 }
 
-
+# RAM based
 resource "aws_autoscaling_policy" "RAM_based_scale_up" {
-  count                  = var.asg_RAM_based_scale_up_policy ? 1 : 0
+  count                  = var.asg_RAM_based_scale_updown_policy ? 1 : 0
   name                   = "${var.app_name}-asg-RAM-scale-up-policy"
   autoscaling_group_name = module.asg.autoscaling_group_name
   adjustment_type        = "ChangeInCapacity"
@@ -129,27 +197,8 @@ resource "aws_autoscaling_policy" "RAM_based_scale_up" {
   policy_type            = "SimpleScaling"
 }
 
-resource "aws_cloudwatch_metric_alarm" "RAM_based_scale_up_alarm" {
-  count               = var.asg_RAM_based_scale_up_policy ? 1 : 0
-  alarm_name          = "${var.app_name}-asg-scale-up-alarm"
-  alarm_description   = "asg-scale-up-cpu-alarm"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "mem_used_percent"
-  namespace           = "AWS/EC2"
-  period              = "120"
-  statistic           = "Average"
-  threshold           = var.threshold_to_scale_up
-  dimensions = {
-    "AutoScalingGroupName" = module.asg.autoscaling_group_name
-  }
-  actions_enabled = true
-  alarm_actions   = [aws_autoscaling_policy.RAM_based_scale_up[0].arn]
-  depends_on      = [aws_autoscaling_policy.RAM_based_scale_up]
-}
-
 resource "aws_autoscaling_policy" "RAM_based_scale_down" {
-  count                  = var.asg_RAM_based_scale_down_policy ? 1 : 0
+  count                  = var.asg_RAM_based_scale_updown_policy ? 1 : 0
   name                   = "${var.app_name}-asg-RAM-scale-down-policy"
   autoscaling_group_name = module.asg.autoscaling_group_name
   adjustment_type        = "ChangeInCapacity"
@@ -158,29 +207,98 @@ resource "aws_autoscaling_policy" "RAM_based_scale_down" {
   policy_type            = "SimpleScaling"
 }
 
+resource "aws_cloudwatch_metric_alarm" "RAM_based_scale_up_alarm" {
+  count               = var.asg_RAM_based_scale_updown_policy ? 1 : 0
+  alarm_name          = "${var.app_name}-asg-scale-up-alarm"
+  alarm_description   = "asg-scale-up-cpu-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "RAM_used_percent"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = var.RAM_threshold_to_scale_up
+  dimensions = {
+    "AutoScalingGroupName" = module.asg.autoscaling_group_name
+  }
+  actions_enabled = true
+  alarm_actions   = [aws_autoscaling_policy.RAM_based_scale_up[0].arn]
+  depends_on      = [aws_autoscaling_policy.RAM_based_scale_up]
+}
+
 resource "aws_cloudwatch_metric_alarm" "scale_down_alarm" {
-  count               = var.asg_RAM_based_scale_down_policy ? 1 : 0
+  count               = var.asg_RAM_based_scale_updown_policy ? 1 : 0
   alarm_name          = "${var.app_name}-asg-scale-down-alarm"
   alarm_description   = "asg-scale-down-cpu-alarm"
   comparison_operator = "LessThanOrEqualToThreshold"
   evaluation_periods  = "2"
-  metric_name         = "mem_used_percent"
+  metric_name         = "RAM_used_percent"
   namespace           = "AWS/EC2"
   period              = "120"
   statistic           = "Average"
-  threshold           = var.threshold_to_scale_down
+  threshold           = var.RAM_threshold_to_scale_down
   dimensions = {
     "AutoScalingGroupName" = module.asg.autoscaling_group_name
   }
   actions_enabled = true
   alarm_actions   = [resource.aws_autoscaling_policy.RAM_based_scale_down[0].arn]
-  depends_on = [
-    aws_autoscaling_policy.RAM_based_scale_down
-  ]
+  depends_on      = [aws_autoscaling_policy.RAM_based_scale_down]
 }
 
 
-  
+
+#SQS
+resource "aws_autoscaling_policy" "customized_metric_specification" {
+  count                  = var.asg_SQS_based_policy ? 1 : 0
+  autoscaling_group_name = module.asg.autoscaling_group_name
+  name                   = "${var.app_name}-customized-metric-SQS-policy"
+  policy_type            = "TargetTrackingScaling"
+  target_tracking_configuration {
+    target_value = var.target_value_SQS
+    customized_metric_specification {
+      metrics {
+        label = "Get the queue size (the number of messages waiting to be processed)"
+        id    = "m1"
+        metric_stat {
+          metric {
+            namespace   = "AWS/SQS"
+            metric_name = "ApproximateNumberOfMessagesVisible"
+            dimensions {
+              name  = "QueueName"
+              value = "my-queue"
+            }
+          }
+          stat = "Sum"
+        }
+        return_data = false
+      }
+      metrics {
+        label = "Get the group size (the number of InService instances)"
+        id    = "m2"
+        metric_stat {
+          metric {
+            namespace   = "AWS/AutoScaling"
+            metric_name = "GroupInServiceInstances"
+            dimensions {
+              name  = "AutoScalingGroupName"
+              value = module.asg.autoscaling_group_name
+            }
+          }
+          stat = "Average"
+        }
+        return_data = false
+      }
+      metrics {
+        label       = "Calculate the backlog per instance"
+        id          = "e1"
+        expression  = "m1 / m2"
+        return_data = true
+      }
+    }
+  }
+}
+
+
 
 #ALB Security Group  
 resource "aws_security_group" "alb-sg" {
@@ -221,17 +339,21 @@ resource "aws_security_group" "alb-sg" {
     Environment = var.Environment
     Owner       = var.Owner
   }
-}
+}   
   
 module "alb" {
-  count              = var.alb_enable ? 1 : 0
   source             = "terraform-aws-modules/alb/aws"
   version            = "~> 6.0"
-  name               = format("%s-%salb", var.Environment, var.app_name)
+  name               = format("%s-%s-alb", var.Environment, var.app_name)
   load_balancer_type = var.load_balancer_type
   vpc_id             = var.vpc_id
   subnets            = var.public_subnets
   security_groups    = [aws_security_group.alb-sg.id]
+  enable_http2       = true
+
+  access_logs = {
+    bucket = "${var.app_name}-access-logs"
+  }
 
   target_groups = [
     {
@@ -240,15 +362,20 @@ module "alb" {
       backend_port          = var.backend_port
       target_type           = var.target_type
       health_check          = {
-        enabled             = var.enabled
+        enabled             = true
         interval            = var.interval
         path                = "/"
         port                = "traffic-port"
-        healthy_threshold   = var.healthy_threshold
-        unhealthy_threshold = var.unhealthy_threshold
+        healthy_threshold   = 2
+        unhealthy_threshold = 3
         timeout             = 4
         protocol            = var.protocol
         matcher             = var.matcher
+      },
+      stickiness = {
+      enabled         = var.stickiness_enabled
+      cookie_duration = var.cookie_duration
+      type            = var.stickiness_type
       }
     }
   ]
@@ -257,10 +384,12 @@ module "alb" {
     {
       port               = 443
       protocol           = "HTTPS"
-      certificate_arn    = var.cert_enable ? module.acm[0].acm_certificate_arn: var.certificate_arn
+      certificate_arn    = module.acm.acm_certificate_arn
       target_group_index = 0
     }
   ]
+
+
   http_tcp_listeners = [
     {
       port        = 80
@@ -281,9 +410,40 @@ module "alb" {
   }
 }
 
+module "s3_bucket_alb_access_logs" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 3.7.0"
+
+  bucket = "${var.app_name}-access-logs"
+  lifecycle_rule = [
+    {
+      id      = "monthly_retention"
+      prefix  = "/"
+      enabled = true
+
+      expiration = {
+        days = 10
+      }
+    }
+  ]
+   versioning = {
+    enabled = true
+  }
+
+  force_destroy = true
+
+  attach_elb_log_delivery_policy = true
+  attach_lb_log_delivery_policy  = true
+
+  tags = {
+    Name        = "${var.app_name}-access-logs"
+    Environment = var.Environment
+    Terraform   = true
+    Owner       = var.Owner
+  }
+}
+
 module "route53-record" {
-  count           = var.route_enable ? 1 : 0
-  #depends_on      = [module.alb[0]]
   allow_overwrite = true
   source          = "clouddrove/route53-record/aws"
   version         = "1.0.1"
@@ -291,15 +451,14 @@ module "route53-record" {
   name            = "${var.app_name}.${var.domain_name}"
   type            = "A"
   alias = {
-    name                   = var.alb_enable ? module.alb[0].lb_dns_name : var.lb_dnsname
-    zone_id                = var.alb_enable ? module.alb[0].lb_zone_id  : var.hosted_zone_id
+    name                   = module.alb.lb_dns_name 
+    zone_id                = module.alb.lb_zone_id 
     evaluate_target_health = true
   }
 }
 
 
 module "acm" {
-  count   = var.cert_enable ? 1 : 0
   source  = "terraform-aws-modules/acm/aws"
   version = "~> 4.0"
 
